895d02a22a343f6832f81473ebadeb4b214166eb
kakatake515 <kakatake515@gmail.com>
2019-10-29 20:18:22 -0400

    Now allows login using email or username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
   end
 
   def signup
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,57 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :trackable, :rememberable, :validatable, :authentication_keys => [:username]
+         :recoverable, :trackable, :rememberable, :validatable
   validates_uniqueness_of :username
+  attr_accessor :login
   has_many :scores, dependent: :destroy
+
+  def self.find_for_database_authentication warden_conditions
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+  end
   
-  def email_required?
-    false
+  protected
+
+  # Attempt to find a user by it's email. If a record is found, send new
+  # password instructions to it. If not user is found, returns a new user
+  # with an email not found error.
+  def self.send_reset_password_instructions attributes = {}
+    recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    recoverable.send_reset_password_instructions if recoverable.persisted?
+    recoverable
   end
 
-  def will_save_change_to_email?
-    false
+  def self.find_recoverable_or_initialize_with_errors required_attributes, attributes, error = :invalid
+    (case_insensitive_keys || []).each {|k| attributes[k].try(:downcase!)}
+
+    attributes = attributes.slice(*required_attributes)
+    attributes.delete_if {|_key, value| value.blank?}
+
+    if attributes.keys.size == required_attributes.size
+      if attributes.key?(:login)
+        login = attributes.delete(:login)
+        record = find_record(login)
+      else
+        record = where(attributes).first
+      end
+    end
+
+    unless record
+      record = new
+
+      required_attributes.each do |key|
+        value = attributes[key]
+        record.send("#{key}=", value)
+        record.errors.add(key, value.present? ? error : :blank)
+      end
+    end
+    record
   end
 
-  def email_changed?
-    false
+  def self.find_record login
+    where(["username = :value OR email = :value", {value: login}]).first
   end
+  
 end
