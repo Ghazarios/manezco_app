133b44f4c07534defc853c402c06ee36ab32b07b
haseeb <haseeb@yallamotor.com>
2019-11-24 16:21:41 +0500

    Changes

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,5 +2,5 @@ class Subject < ApplicationRecord
   belongs_to :course
   has_many :quizzes
   has_many :child_subjects, class_name: 'Subject', foreign_key: 'parent_id'
-	belongs_to :parent_subject, class_name: 'Subject', foreign_key: 'parent_id'
+	belongs_to :parent_subject, class_name: 'Subject', foreign_key: 'parent_id',optional: true
 end
